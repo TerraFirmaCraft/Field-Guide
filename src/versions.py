f9eaee3c992f988e927965ca5d5b4701a5a4c866
Alex O'Neill <35673674+alcatrazEscapee@users.noreply.github.com>
2022-09-12 14:40:05 -0400

    v2.1.6-beta

--- src/versions.py
+++ src/versions.py
@@ -1,8 +1,8 @@
 
-VERSION = 'v2.1.4-beta'
+VERSION = 'v2.1.6-beta'
 MC_VERSION = '1.18.2'
 FORGE_VERSION = '40.1.73'
-LANGUAGES = ['en_us', 'ko_kr']
+LANGUAGES = ['en_us', 'ko_kr', 'zh_cn']
 
 
 if __name__ == '__main__':
