2604c3763e73bf977ac0236a797988db60d4ca9c
AlcatrazEscapee <alex@molleroneill.com>
2023-02-28 19:56:46 -0500

    v2.2.9 + uk_ua

--- src/versions.py
+++ src/versions.py
@@ -1,8 +1,8 @@
 
-VERSION = 'v2.2.4'
+VERSION = 'v2.2.9'
 MC_VERSION = '1.18.2'
 FORGE_VERSION = '40.1.73'
-LANGUAGES = ['en_us', 'ko_kr', 'zh_cn', 'zh_tw']
+LANGUAGES = ['en_us', 'ko_kr', 'uk_ua', 'zh_cn', 'zh_tw']
 
 
 if __name__ == '__main__':
