8e830f21edbd071bf54cb0232635605cd279b87f
AlcatrazEscapee <alex@molleroneill.com>
2023-04-30 12:07:08 -0400

    v2.2.17, and zh_hk

--- src/versions.py
+++ src/versions.py
@@ -10,10 +10,10 @@ class Addon(NamedTuple):
     def book_dir(self) -> str:
         return 'addons/%s-%s/%s/data/%s/patchouli_books/field_guide/' % (self.mod_id, self.version, self.resource_path, self.mod_id)
 
-VERSION = 'v2.2.16'
+VERSION = 'v2.2.17'
 MC_VERSION = '1.18.2'
 FORGE_VERSION = '40.1.73'
-LANGUAGES = ['en_us', 'pt_br', 'ko_kr', 'uk_ua', 'zh_cn', 'zh_tw']
+LANGUAGES = ['en_us', 'pt_br', 'ko_kr', 'uk_ua', 'zh_cn', 'zh_hk', 'zh_tw']
 
 ADDONS = [
     Addon('eerussianguy', 'firmalife', 'v1.2.9', 'firmalife', 'src/main/resources'),
