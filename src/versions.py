e74c4d5bde8be87972ac358e9dca96254f615bd6
MrHiTech123 <mlebowitz97@gmail.com>
2025-08-09 22:38:39 -0400

    Added bsa capability for knapping_recipe pages, added bsa-1.2.1

--- src/versions.py
+++ src/versions.py
@@ -50,6 +50,7 @@ ADDONS = (
     Addon('HyperDashPony', 'FirmaCiv', '0.2.5-alpha-1.20.1', 'firmaciv', 'src/main/resources'),
     Addon('eerussianguy', 'beneath', 'v1.0', 'beneath', 'src/main/resources'),
     Addon('MrHiTech123', 'Artisanal', '1.3.1', 'artisanal', 'src/main/resources'),
+    Addon('MrHiTech123', 'BetterStoneAge', '1.2.1', 'bsa', 'src/main/resources'),
     Addon('Therighthon', 'ArborFirmaCraft', 'v1.0.13', 'afc', 'src/main/resources'),
     Addon('Therighthon', 'RoadsAndRoofsTFC', 'v0.2.0', 'rnr', 'src/main/resources'),
     Addon('redstoneguy10ls', 'lithiccoins', '1.1.1', 'lithiccoins', 'src/main/resources'),
