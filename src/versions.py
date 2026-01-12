402afa3a9167a21815e23032e02ab4c48fe31230
eerussianguy <eerussianguy@gmail.com>
2026-01-12 18:32:45 -0500

    update versions.py

--- src/versions.py
+++ src/versions.py
@@ -62,7 +62,7 @@ class OldVersion(NamedTuple):
 # )
 
 
-VERSION = 'v4.0.16-beta'
+VERSION = 'v4.0.17-beta'
 MC_VERSION = '1.21.1'
 FORGE_VERSION = '21.1.197'
 LANGUAGES = ('en_us', 'ja_jp', 'pt_br', 'ko_kr', 'uk_ua', 'zh_cn', 'zh_hk', 'zh_tw', 'ru_ru')
